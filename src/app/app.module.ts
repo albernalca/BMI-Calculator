46d71f0eecffbd257f5a15ef729ce9025d12b274
albernalca <albernalca@gmail.com>
2023-04-09 06:27:08 -0500

    Creo Componentes inicio y resultado, configuro bootstrap, ordeno en app.module.ts los componentes y angular material, renderizo el componente inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Componentes
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
+
 //Angular Material
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
